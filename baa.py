19630cfcd94687bcc4a3cccaf21d220b8f58d1bd
deboutlesdamnes <jasonzhao512@gmail.com>
2023-08-03 16:52:32 -0700

    add llamaindex processor

---
+++ baa.py
@@ -0,0 +1,19 @@
+from llama_index import Document, SimpleDirectoryReader, VectorStoreIndex
+from llama_index.node_parser import SimpleNodeParser
+import glob
+import os
+
+documents = SimpleDirectoryReader('./billtexts').load_data()
+parser = SimpleNodeParser()
+nodes = parser.get_nodes_from_documents(documents)
+index = VectorStoreIndex(nodes)
+index.storage_context.persist(persist_dir="<persist_dir>")
+
+text_list = []
+text_path = r'./billtexts'
+for file in os.listdir(text_path):
+    if file.endswith('.txt'):
+        text_list.append(file)
+
+documents = [Document(text=t) for t in text_list]
+
