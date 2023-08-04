0300c00987f0bd43d00066d3ec276d8d679d46b3
deboutlesdamnes <jasonzhao512@gmail.com>
2023-08-03 19:11:03 -0700

    Testing huggingface model

--- baa.py
+++ baa.py
@@ -1,8 +1,26 @@
 from llama_index import Document, LangchainEmbedding, SimpleDirectoryReader, VectorStoreIndex, ServiceContext, set_global_service_context
+from llama_index.llms import HuggingFaceLLM
+from llama_index.prompts.prompts import SimpleInputPrompt
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from llama_index.node_parser import SimpleNodeParser
+import torch
 import glob
 import os
+
+query_wrapper_prompt = SimpleInputPrompt("<|USER|>{query_str}<|ASSISTANT|>")
+
+llm = HuggingFaceLLM(
+    context_window=4096, 
+    max_new_tokens=256,
+    generate_kwargs={"temperature": 0.7, "do_sample": False},
+    query_wrapper_prompt=query_wrapper_prompt,
+    tokenizer_name="TheBloke/Llama-2-70B-GGML",
+    model_name="TheBloke/Llama-2-70B-GGML",
+    device_map="auto",
+    stopping_ids=[50278, 50279, 50277, 1, 0],
+    tokenizer_kwargs={"max_length": 4096},
+    model_kwargs={"torch_dtype": torch.float16}
+)
 
 embed_model = LangchainEmbedding(
   HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
